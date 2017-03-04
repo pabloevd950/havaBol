622b0e3bafa8a24ee380fd810b4234bbf9302958
pabloevd950 <pabloevd950@gmail.com>
2017-03-03 20:01:58 -0600

    Utilities & ParserException

---
+++ src/havabol/Utilities.java
@@ -0,0 +1,60 @@
+package havabol;
+
+
+/**
+ * Created  on 3/3/17.
+ */
+public class Utilities
+{
+
+
+    public static int addInt(int x, int y){
+         return x+y;
+    }
+
+    public static double addFloat(double x, double y){
+        return x+y;
+    }
+
+    public static int subInt(int x, int y){
+        return x-y;
+    }
+
+    public static double subFloat(double x, double y){
+        return x-y;
+    }
+    public static int divInt(int x, int y){
+        return x/y;
+    }
+
+    public static double divFloat(double x, double y){
+        return x/y;
+    }
+
+    public static int mulInt(int x, int y){
+        return x*y;
+    }
+
+    public static double mulFloat(double x, double y){
+        return x*y;
+    }
+
+    public static double expInt(int x, int y){
+        return Math.pow(x,y);
+    }
+
+    public static double expDouble(double x, double y){
+        return Math.pow(x,y);
+    }
+
+    public static  < T extends Comparable > boolean isEqual(T x, T y){
+        if(x.compareTo(y) > 0)
+            return true;
+        else
+        return false;
+    }
+    //public static boolean isGreat
+
+
+
+}
